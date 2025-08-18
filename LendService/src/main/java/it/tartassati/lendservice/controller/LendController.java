9dcebd7b644b521660a4a8b95158c2ba122cc682
Gabriele Banchio <gabriele.banchio2001@gmail.com>
2025-08-18 22:15:41 +0200

    dockerizzazione servizio e db completata

--- LendService/src/main/java/it/tartassati/lendservice/controller/LendController.java
+++ LendService/src/main/java/it/tartassati/lendservice/controller/LendController.java
@@ -3,6 +3,7 @@ package it.tartassati.lendservice.controller;
 import it.tartassati.lendservice.model.Lend;
 import it.tartassati.lendservice.service.LendService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +16,15 @@ public class LendController {
     public LendController(LendService lendService) {
         this.lendService = lendService;
     }
+    @GetMapping()
+    public void createRndLend() {
+        Lend lend = new Lend();
 
+        lend.setClientId(1L);
+        lendService.createLend(lend);
+    }
     @GetMapping("/{readerId}")
-    public List<Lend> getReaderLends(Long readerId) {
+    public List<Lend> getReaderLends(@PathVariable Long readerId) {
         return lendService.getLendsByReader(readerId);
     }
 }
--- LendService/src/main/java/it/tartassati/lendservice/model/Lend.java
+++ LendService/src/main/java/it/tartassati/lendservice/model/Lend.java
@@ -13,6 +13,62 @@ import java.sql.Date;
 public class Lend {
 
 
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isPhysical() {
+        return isPhysical;
+    }
+
+    public void setPhysical(boolean physical) {
+        isPhysical = physical;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Date getEndOfLend() {
+        return endOfLend;
+    }
+
+    public void setEndOfLend(Date endOfLend) {
+        this.endOfLend = endOfLend;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
